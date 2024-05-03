84c590c8cf6de21aa0d79b65d7a592e69cd0c5c8
Tom Anderson <tom@voxel51.com>
2024-05-03 11:35:57 -0400

    updating to run without 1.6.1 additions

--- cas_helpers/__init__.py
+++ cas_helpers/__init__.py
@@ -1,1 +1,1 @@
-from cas_helpers.cas_methods import add_org, add_user
+from cas_helpers.cas_methods import add_org, add_user, get_existing_auth_config
--- migrate.py
+++ migrate.py
@@ -9,12 +9,12 @@ import asyncio
 
 from auth0_helpers import Auth0ManagementAPIFactory, Auth0Manager
 from config import Config
-from cas_helpers import add_org, add_user
+from cas_helpers import add_org, add_user, get_existing_auth_config
 
 auth0_mgmt_factory = Auth0ManagementAPIFactory(
     Config.CLIENT_DOMAIN,
     Config.CLIENT_ID,
-    Config.CLIENT_SECRET,
+    Config.CLIENT_MGMT_SECRET,
     Config.AUDIENCE,
 )
 
@@ -34,10 +34,17 @@ async def migrate_organization(session):
     await add_org(session, dict(org))
 
 async def main():
-    session = aiohttp.ClientSession()
-    await migrate_organization(session)
-    await migrate_users(session)
-    await session.close()
+    async with aiohttp.ClientSession() as session:
+        auth_config = await get_existing_auth_config(session)
+        await migrate_organization(session)
+        await migrate_users(session)
+
+        if not auth_config:
+            print("==== Warning ====")
+            print("An existing auth configuration was not found")
+            print("or does not match an existing IdP configuration.\n")
+            print("Please review your auth configuration in the provided")
+            print("page at: {your domain}/cas/admins")
     print("Migration Complete")
 
 asyncio.run(main())
